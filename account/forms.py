026e9010eee906d22149d63a098292768fa9dbdf
BY1994 <gywlsangel@naver.com>
2019-05-15 00:35:33 +0000

    login page

--- account/forms.py
+++ account/forms.py
@@ -8,9 +8,25 @@ from .models import Profile
 class UserProfileForm(forms.ModelForm):
   class Meta:
     model = Profile
-    fields = ['nickname','genre_Action','genre_Adventure','genre_Animation','genre_Comedy','genre_Crime','genre_Documentary','genre_Drama','genre_Family','genre_Fantasy','genre_Horror','genre_Music','genre_Mystery','genre_Romance','genre_SF','genre_Thriller','genre_War','genre_Western',]
+    fields = ['genre_Action','genre_Adventure','genre_Animation','genre_Comedy','genre_Crime','genre_Documentary','genre_Drama','genre_Family','genre_Fantasy','genre_Horror','genre_Music','genre_Mystery','genre_Romance','genre_SF','genre_Thriller','genre_War','genre_Western',]
     labels = {
-        'nickname': '닉네임',
+      'genre_Action':'액션',
+      'genre_Adventure':'어드벤처',
+      'genre_Animation':'애니메이션',
+      'genre_Comedy':'코미디',
+      'genre_Crime':'범죄',
+      'genre_Documentary':'다큐멘터리',
+      'genre_Drama':'드라마',
+      'genre_Family':'가족',
+      'genre_Fantasy':'판타지',
+      'genre_Horror':'공포',
+      'genre_Music':'음악',
+      'genre_Mystery':'미스터리',
+      'genre_Romance':'로맨스',
+      'genre_SF':'SF',
+      'genre_Thriller':'스릴러',
+      'genre_War':'전쟁',
+      'genre_Western':'서부',
     }
     
 class UserCreationMultiForm(MultiModelForm):
--- account/views.py
+++ account/views.py
@@ -1,33 +1,37 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth import login as auth_login
 from django.contrib.auth import logout as auth_logout
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from .forms import UserCreationMultiForm
+from .forms import UserProfileForm
 
 # Create your views here.
 def login(request):
   if request.method == "POST":
-    form = AuthenticationForm(request, request.POST)
-    if form.is_valid():
-      auth_login(request, form.get_user())
-    return redirect('posts:list')
+    forms = AuthenticationForm(request, request.POST)
+    if forms.is_valid():
+      auth_login(request, forms.get_user())
+    return redirect('home')
   else:
-    form = AuthenticationForm()
-    return render(request, 'account/login.html', {'form':form})
+    forms = AuthenticationForm()
+    return render(request, 'account/login.html', {'forms':forms})
 
 def logout(request):
   auth_logout(request)
-  return redirect('posts:list')
+  return redirect('home')
 
 def signup(request):
   if request.method == "POST":
-    form = UserCreationMultiForm(request.POST)
-    if form.is_valid():
+    # 넘어온 거 회원가입 체크할 때는 user form이랑 profile form이랑 둘 다 체크 필요
+    forms = UserCreationMultiForm(request.POST) #forms = UserProfileForm(request.POST)
+    if forms.is_valid():
       user = form.save()
       auth_login(request, user)
       return redirect('home')
   else:
-    form = UserCreationMultiForm()
-  return render(request, 'account/signup.html', {'form' : form})
+    # 보내줄 때는 이 form만 있어도 됨
+    forms = UserProfileForm()
+  return render(request, 'account/signup.html', {'forms' : forms})
 
 def delete(request):
   if request.method == "POST":
