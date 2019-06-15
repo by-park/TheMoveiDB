4a3dff3211e8d06a0d45cfb8e4362210ed74c037
Boyoun Park <lovelychoco100@gmail.com>
2019-06-15 08:23:47 +0000

    settings.py 잘못 삽입한 코드 삭제

--- The_Movei_DB/settings.py
+++ The_Movei_DB/settings.py
@@ -127,6 +127,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
-AUTH_USER_MODEL = "accounts.User"
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 django_heroku.settings(locals())
