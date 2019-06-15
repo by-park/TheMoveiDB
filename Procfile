0e2f5f039df4c81972386c1bc05493c81c66bbb1
Boyoun Park <lovelychoco100@gmail.com>
2019-06-15 08:20:16 +0000

    heroku 배포 준비

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn The_Movei_DB.wsgi
--- The_Movei_DB/settings.py
+++ The_Movei_DB/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -31,8 +32,8 @@ ALLOWED_HOSTS = ["*"]
 # Application definition
 
 INSTALLED_APPS = [
-  'dal',
-  'dal_select2',
+#   'dal',
+#   'dal_select2',
   'django.contrib.admin',
   'django.contrib.auth',
   'django.contrib.contenttypes',
@@ -126,3 +127,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+AUTH_USER_MODEL = "accounts.User"
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+django_heroku.settings(locals())
